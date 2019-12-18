7441b838260be7ea8105132233fcf01d82ddaeb4
MostAardvark <54926720+MostAardvark639@users.noreply.github.com>
2019-12-18 14:49:58 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -158,10 +158,10 @@ function draw() {
 
 				push()
 				imageMode(CENTER)
-				image(brother_jay, poses[0].pose.rightKnee.x, 150, windowWidth / 2, windowHeight / 2)
+				image(brother_jay, poses[0].pose.rightShoulder.x, 150, windowWidth / 2, windowHeight / 2)
 				image(sister_noah, poses[0].pose.leftWrist.x, 150, windowWidth / 2, windowHeight /2)
 				image(mother, poses[0].pose.rightWrist.x, 150, windowWidth / 3, windowHeight / 3)
-				image(father, poses[0].pose.leftKnee.x, 150, windowWidth / 3, windowHeight / 3)
+				image(father, poses[0].pose.leftShoulder.x, 150, windowWidth / 3, windowHeight / 3)
 				pop()
 
 		//TORSO
