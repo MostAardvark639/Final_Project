e41818a786af0cd3cf670ac721538d0309e34323
MostAardvark <54926720+MostAardvark639@users.noreply.github.com>
2019-12-17 17:32:32 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ let head
 function preLoad(){
 head = loadImage('FACEBOOK_PNG.png')
 spiderman_cave_in = loadImage('spiderman_cave_in.jpg')
-spidey_mask = loadImage('SPIDEY_MASK_PNG.png')
+SPIDEY_MASK_PNG = loadImage('SPIDEY_MASK_PNG.png')
 
 }
 
